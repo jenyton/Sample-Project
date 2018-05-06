b76ce8e831a568a6bf225a39503231e476a5d74c
jenyton <38453066+jenyton@users.noreply.github.com>
2018-05-06 23:01:32 +0530

    sting formatting and operations

---
+++ string_properties.py
@@ -0,0 +1,20 @@
+string_mul="a"*10
+print string_mul
+x="Hi this is a sting"
+print x.split()
+print x.split('i')
+print ("This is a sting {}".format("INSERTED"))
+print ("Favourite Fruits are {}, {}, {}".format("Apple","Banana","Mango"))
+print ("Favourite Fruits are {2}, {0}, {1}".format("Apple","Banana","Mango"))
+result=100/777
+print float(result)
+result=108.34578
+print ("the Result was {r}".format(r=result))
+#{value:width.precision f}
+print ("the Result was {r:1.3f}".format(r=result))
+##fsting -- python 3
+#name="jeny"
+#print (f'hello,{name}')
+
+
+
