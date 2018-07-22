a65cfa0e0cd77eaf7f73c0c71df54fd9ffc2237d
jenyton <38453066+jenyton@users.noreply.github.com>
2018-07-22 19:12:07 +0530

    adding more to function passing concept

--- func_passing_concept.py
+++ func_passing_concept.py
@@ -16,6 +16,21 @@ def hello(name="Jose"):
 
 a=hello()
 ##Calling a function inside a function by assigning the function to a variable
+print "Function assignment"
 print a
 print a()
+b=hello
+print b
+b()
+
+def hi():
+	return "Hi !!!"
+
+def other(some_def_func):
+	print "Other code runs here"
+	print(some_def_func())
+
+
+print ("\n\nCalling a function by passing another function as argument")
+other(hi)
 
